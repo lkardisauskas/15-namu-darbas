34cb829a177742520ed6d4170639c5351bfaf3ed
lkardisauskas <109983497+lkardisauskas@users.noreply.github.com>
2022-09-18 19:46:59 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+var tabs = document.querySelectorAll(".tab")
+var buttons = document.querySelectorAll("button")
+
+function showTab(event, id){
+    for(var x of tabs){
+        x.style.display = "none"
+        x.style.opacity = "0"
+    }
+    document.getElementById(id).style.display = "block"
+
+    setTimeout(function(){
+        document.getElementById(id).style.opacity = "1"
+    }, 300)
+
+    setTimeout(function(){
+        document.getElementById(id).style.marginTop = "100px"
+    }, 500)
+
+
+    // Button style
+    for(var x of buttons){
+        x.classList.remove("selected")
+    }
+    event.currentTarget.classList.add("selected")
+}
+    
